4a32e75b6041d4cd32c7831ec44118f633e2a5e2
David L. Inman <davidlevere@gmail.com>
2024-07-05 19:04:11 -0400

    seq impl

--- src/core/traverse/Seq.cs
+++ src/core/traverse/Seq.cs
@@ -7,6 +7,12 @@ public interface ISeqable {
 
 public static class Seq {
     public static IEnumerable<ISeqable> ToSeq(this ISeqable target) {
-        
+        var s = new List<ISeqable> { target };
+        while(s.Count > 0) {
+            var t = s[^1];
+            var ns = t.Next();
+            s.AddRange(ns);
+            yield return t;
+        }
     }
 }
