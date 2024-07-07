0d84be6978161f63477554d1e3a62bfeaa33c5e2
David L. Inman <davidlevere@gmail.com>
2024-07-07 19:11:00 -0400

    constructor methods for patterns

--- src/core/traverse/Pattern.cs
+++ src/core/traverse/Pattern.cs
@@ -24,11 +24,28 @@ public abstract record Pattern<T> where T : IMatchable<T> {
     public record SubContentPath(IEnumerable<Pattern<T>> Ps) : Pattern<T>;
 
     public record Predicate(Func<T, bool> Pred) : Pattern<T>;
-    public record MatchWith(Func<IDictionary<string, T>, Pattern<T>> With) : Pattern<T>;
+    public record MatchWith(Func<IDictionary<string, T>, Pattern<T>> Func) : Pattern<T>;
 }
 
 public static class Pattern {
     public static Pattern<T> Wild<T>() where T : IMatchable<T> => new Pattern<T>.Wild();
+
+    public static Pattern<T> Capture<T>(string name) where T : IMatchable<T> => new Pattern<T>.Capture(name);
+    public static Pattern<T> TemplateVar<T>(string name) where T : IMatchable<T> => new Pattern<T>.TemplateVar(name);
+
+    public static Pattern<T> Exact<T>(Type id, IEnumerable<Pattern<T>> contents) where T : IMatchable<T> => new Pattern<T>.Exact(id, contents);
+    public static Pattern<T> Contents<T>(IEnumerable<Pattern<T>> contents) where T : IMatchable<T> => new Pattern<T>.Contents(contents);
+
+    public static Pattern<T> And<T>(Pattern<T> left, Pattern<T> right) where T : IMatchable<T> => new Pattern<T>.And(left, right);
+    public static Pattern<T> Or<T>(Pattern<T> left, Pattern<T> right) where T : IMatchable<T> => new Pattern<T>.Or(left, right);
+
+
+    public static Pattern<T> PathNext<T>() where T : IMatchable<T> => new Pattern<T>.PathNext();
+    public static Pattern<T> Path<T>(IEnumerable<Pattern<T>> patterns) where T : IMatchable<T> => new Pattern<T>.Path(patterns);
+    public static Pattern<T> SubContentPath<T>(IEnumerable<Pattern<T>> patterns) where T : IMatchable<T> => new Pattern<T>.SubContentPath(patterns);
+
+    public static Pattern<T> Predicate<T>(Func<T, bool> predicate) where T : IMatchable<T> => new Pattern<T>.Predicate(predicate);
+    public static Pattern<T> MatchWith<T>(Func<IDictionary<string, T>, Pattern<T>> func) where T : IMatchable<T> => new Pattern<T>.MatchWith(func);
 
     public static IEnumerable<(string Name, T Item)> Find<T>(this IMatchable<T> data, Pattern<T> pattern) where T : IMatchable<T> {
         throw new NotImplementedException();
