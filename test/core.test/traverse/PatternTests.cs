a94f9bdafa159894d06a8a5300ddaf736d4f2aa6
David L. Inman <davidlevere@gmail.com>
2024-07-07 19:04:48 -0400

    setup for pattern tests

--- test/core.test/traverse/PatternTests.cs
+++ test/core.test/traverse/PatternTests.cs
@@ -1,9 +1,38 @@
 
 using csr.core.traverse;
+using static csr.core.traverse.Pattern;
 
 namespace csr.test.core.traverse;
 
 [TestFixture]
 public class PatternTests {
 
+    [Test]
+    public void FindWithWild() { 
+        var t = new Tree.Leaf(7);
+        var output = t.Find(Wild<Tree>());
+        // TODO 
+    }
+
+    private abstract record Tree : IMatchable<Tree> {
+        private Tree() { }
+
+        public record Leaf(byte Value) : Tree;
+        public record Node(Tree Left, Tree Right) : Tree;
+
+        public void Deconstruct(out Type id, out IEnumerable<Tree> contents) {
+            switch (this) {
+                case Leaf l:
+                    id = typeof(Leaf);
+                    contents = [];
+                    break;
+                case Node n:
+                    id = typeof(Node);
+                    contents = [n.Left, n.Right];
+                    break;
+                default:
+                    throw new NotImplementedException($"Unknown {nameof(Tree)} case {this.GetType()}");
+            }
+        }
+    }
 }
