bca8194ec49dec3923e8bc2a867394b4a8d64687
David L. Inman <davidlevere@gmail.com>
2024-07-25 23:27:15 -0400

    inc prog csharp ast

--- src/core/code/CSharp.cs
+++ src/core/code/CSharp.cs
@@ -16,9 +16,21 @@ public abstract record CSharpAst : IMatchable<CSharpAst> {
                 id = typeof(CSharpAst.Symbol);
                 contents = [];
                 break;
-            case CSharpAst.Class x:
+            case CSharpAst.CSType.SimpleType:
+                id = typeof(CSharpAst.CSType.SimpleType);
+                contents = [];
+                break;
+            case CSharpAst.CSType.IndexedType x:
+                id = typeof(CSharpAst.CSType.IndexedType);
+                contents = x.Types;
+                break;
+            case CSharpAst.SuperTypeList x:
+                id = typeof(CSharpAst.SuperTypeList);
+                contents = x.SuperTypes;
+                break;
+            case CSharpAst.Class(var name, var superTypes, var members):
                 id = typeof(CSharpAst.Class);
-                contents = [x.Name]; // TODO
+                contents = [name, superTypes, members]; 
                 break;
             default:
                 throw new NotImplementedException($"Unknown {nameof(CSharpAst)} case {this.GetType()}");
@@ -26,9 +38,25 @@ public abstract record CSharpAst : IMatchable<CSharpAst> {
     }
     
     public record Symbol(string Value) : CSharpAst;
-    // TODO super type list
-    // Symbol<Indexed>
-    public record Class(Symbol Name, ImmutableList<Symbol> SuperTypes, ImmutableList<CSharpAst> Contents) : CSharpAst;
+
+    public abstract record CSType : CSharpAst {
+        private CSType() { }
+
+        public record SimpleType(string Name) : CSType;
+        public record IndexedType(string Name, ImmutableList<CSType> Types) : CSType;
+    }
+
+    public record SuperTypeList(ImmutableList<CSType> SuperTypes) : CSharpAst;
+    
+    public abstract record ClassMember : CSharpAst {
+        private ClassMember() { }
+
+        public record Field(string Name, CSType type) : ClassMember; // TODO initial value (optional), accessibility, readonly, static, etc?
+    }
+
+    public record ClassMemberList(ImmutableList<ClassMember> Members) : CSharpAst;
+
+    public record Class(Symbol Name, SuperTypeList SuperTypes, ClassMemberList Members) : CSharpAst;
 
 }
 
