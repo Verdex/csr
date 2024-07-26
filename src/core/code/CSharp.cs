39a25c44c53a574e7dce29b48b66c7e523b30ab5
David L. Inman <davidlevere@gmail.com>
2024-07-25 22:24:14 -0400

    Add ast in csharp code parser

--- src/core/code/CSharp.cs
+++ src/core/code/CSharp.cs
@@ -2,11 +2,28 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
+using csr.core.traverse;
+
 namespace csr.core.code;
 
-public abstract record CSharpAst {
-    private CSharpAstS() { }
+public abstract record CSharpAst : IMatchable<CSharpAst> {
+    private CSharpAst() { }
 
+    public void Deconstruct(out Type id, out IEnumerable<CSharpAst> contents) {
+        switch (this) {
+            case CSharpAst.Class:
+                id = typeof(CSharpAst.Class);
+                contents = [];
+                break;
+            default:
+                throw new NotImplementedException($"Unknown {nameof(CSharpAst)} case {this.GetType()}");
+        }
+    }
+    
+
+    public record Class() : CSharpAst {
+
+    }
 
 }
 
