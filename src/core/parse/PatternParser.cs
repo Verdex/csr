2df61525a4127d0a3135af686218875e8664c1c1
David L. Inman <davidlevere@gmail.com>
2024-07-25 22:09:41 -0400

    success / failure result

--- src/core/parse/PatternParser.cs
+++ src/core/parse/PatternParser.cs
@@ -3,6 +3,18 @@ using csr.core.traverse;
 
 namespace csr.core.parse;
 
-public class PatternParser {
+public record ParseResult<T> {
+    private ParseResult() { }
 
+    public record Success(T Item) : ParseResult<T>;
+    public record Failure(string Message) : ParseResult<T>;
+}
+
+public static class PatternParser {
+    private static ParseResult<Pattern<T>> Fail<T>(string message) where T : IMatchable<T> => new ParseResult<Pattern<T>>.Failure(message);
+    private static ParseResult<Pattern<T>> Success<T>(Pattern<T> item) where T : IMatchable<T> => new ParseResult<Pattern<T>>.Success(item);
+
+    public static ParseResult<Pattern<T>> Parse<T>(string input) where T : IMatchable<T> {
+        return Fail<T>("blarg");
+    }
 }
