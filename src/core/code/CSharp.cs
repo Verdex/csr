1c042c97db3f356765e839f6ec1017af01d16e0a
David L. Inman <davidlevere@gmail.com>
2024-09-09 23:13:55 -0400

    csharp ast needs to be simple or else it's going to be way too much of an effort to get it to work

--- src/core/code/CSharp.cs
+++ src/core/code/CSharp.cs
@@ -4,48 +4,15 @@ using Microsoft.CodeAnalysis.CSharp;
 using System.Collections.Immutable;
 
 using csr.core.traverse;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace csr.core.code;
 
 
-public abstract record CSharpAst : IMatchable<Type, CSharpAst> {
+public abstract record CSharpAst : IMatchable<string, CSharpAst> {
     private CSharpAst() { }
 
-    public void Deconstruct(out Type id, out IEnumerable<CSharpAst> contents) {
-        switch (this) {
-            case CSharpAst.Symbol:
-                id = typeof(CSharpAst.Symbol);
-                contents = [];
-                break;
-            case CSharpAst.CSType.SimpleType:
-                id = typeof(CSharpAst.CSType.SimpleType);
-                contents = [];
-                break;
-            case CSharpAst.CSType.IndexedType x:
-                id = typeof(CSharpAst.CSType.IndexedType);
-                contents = x.Types;
-                break;
-            case CSharpAst.SuperTypeList x:
-                id = typeof(CSharpAst.SuperTypeList);
-                contents = x.SuperTypes;
-                break;
-            case CSharpAst.Class(var name, var superTypes, var members):
-                id = typeof(CSharpAst.Class);
-                contents = [name, superTypes, members]; 
-                break;
-            default:
-                throw new NotImplementedException($"Unknown {nameof(CSharpAst)} case {this.GetType()}");
-        }
-    }
-    
-    public record Symbol(string Value) : CSharpAst;
-
-    public abstract record CSType : CSharpAst {
-        private CSType() { }
-
-        public record SimpleType(string Name) : CSType;
-        public record IndexedType(string Name, ImmutableList<CSType> Types) : CSType;
-    }
+}
 
     public record SuperTypeList(ImmutableList<CSType> SuperTypes) : CSharpAst;
     
