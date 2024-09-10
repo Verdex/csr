872ab01e30a8ac40cbd9a027dc4a3ed8f1060bdc
David L. Inman <davidlevere@gmail.com>
2024-09-09 23:29:53 -0400

    inc prog

--- src/core/code/CSharp.cs
+++ src/core/code/CSharp.cs
@@ -12,25 +12,49 @@ namespace csr.core.code;
 public abstract record CSharpAst : IMatchable<string, CSharpAst> {
     private CSharpAst() { }
 
-}
-
-    public record SuperTypeList(ImmutableList<CSType> SuperTypes) : CSharpAst;
-    
-    public abstract record ClassMember : CSharpAst {
-        private ClassMember() { }
-
-        public record Field(string Name, CSType type) : ClassMember; // TODO initial value (optional), accessibility, readonly, static, etc?
+    public void Deconstruct(out string id, out IEnumerable<CSharpAst> contents)
+    {
+        throw new NotImplementedException();
     }
 
-    public record ClassMemberList(ImmutableList<ClassMember> Members) : CSharpAst;
+    public sealed record Symbol(string Value) : CSharpAst;
+    public sealed record SyntaxList(ImmutableList<CSharpAst> Contents) : CSharpAst;
 
-    public record Class(Symbol Name, SuperTypeList SuperTypes, ClassMemberList Members) : CSharpAst;
+    // TODO for class: generics, initial constructor, base/interfaces, type constraints,
+    //  nested top level, field, property, events, method
+
+    public sealed record ClassDef(Symbol Name) : CSharpAst;
+
+
+
+    // TODO: class, record, (struct record?), struct, enum, interface, namespace, using, delegate
 
 }
 
 public static class CSharp {
+
     public static void Parse() {
-        var tree = CSharpSyntaxTree.ParseText("class X { }");
+        var tree = CSharpSyntaxTree.ParseText(@"
+            public sealed class X<T>(T x) : Base, Interface where X : class, IThing { 
+                public int J { get; }
+                public int I;
+                public void K() { }
+
+            }");
         var root = tree.GetCompilationUnitRoot();
+        foreach(var w in root.ChildNodes()) {
+
+            Console.WriteLine($"{w.GetType()}");
+
+            foreach(var t in w.ChildNodes()) {
+                Console.WriteLine($"!!!! {t} :: {t.GetType()}");
+            }
+
+            if ( w is ClassDeclarationSyntax h)  {
+                foreach( var x in h.Members ) {
+                    Console.WriteLine(x);
+                }
+            }
+        }
     }
 }
