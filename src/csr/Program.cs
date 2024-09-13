b16aab34ea34d4440b69d1daba14491e9ffa6195
David L. Inman <davidlevere@gmail.com>
2024-09-12 20:11:31 -0400

    inc prog main

--- src/csr/Program.cs
+++ src/csr/Program.cs
@@ -4,8 +4,25 @@ using csr.core.code;
 namespace csr;
 
 public static class Program {
+
     public static void Main() {
-        CSharp.Parse();
-        Console.WriteLine("blarg");
+        var currentDirectory = Directory.GetCurrentDirectory();
+
+        var csFiles = Directory.GetDirectories(currentDirectory, "", SearchOption.AllDirectories)
+            .Where(d => new [] {"Debug", "Release", "bin", "obj", ".git"}.All(target => !IgnoreDir(d, target)))
+            .SelectMany(d => Directory.GetFiles(d))
+            .Where(f => f.EndsWith(".cs"));
+
+        while (true) {
+            Console.Write(">");
+            var line = Console.ReadLine();
+            Console.WriteLine($"{line}");
+        }
+        //CSharp.Parse();
     }
+
+    private static bool IgnoreDir(string dir, string endsWith) 
+        => dir.EndsWith($"{Path.DirectorySeparatorChar}{endsWith}") 
+        || dir.Contains($"{Path.DirectorySeparatorChar}{endsWith}{Path.DirectorySeparatorChar}");
+
 }
