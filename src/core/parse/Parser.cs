9b25ded06c3a5a25832641b8b59fdec718e08ff7
David L. Inman <davidlevere@gmail.com>
2024-09-21 16:46:18 -0400

    nevermind:  fail index should be propogated so that originator can broadcast where things went wrong
    [Afterall, it's going to be obvious that it's a fail and the caller can
    just use whatever index it has on hand]

--- src/core/parse/Parser.cs
+++ src/core/parse/Parser.cs
@@ -16,7 +16,7 @@ public sealed record Parser<T>(Func<string, int, Result<T>> P) {
     public Parser<S> Select<S>(Func<T, S> map) 
         => new Parser<S>((input, index) => P(input, index) switch {
             Result<T>.Succ(var newIndex, var pattern) => new Result<S>.Succ(newIndex, map(pattern)),
-            Result<T>.Fail(_, var message) => new Result<S>.Fail(index, message),
+            Result<T>.Fail(var failIndex, var message) => new Result<S>.Fail(failIndex, message),
             _ => throw new Exception("Encountered unsupported Result in Select"),
         });
 
@@ -24,18 +24,18 @@ public sealed record Parser<T>(Func<string, int, Result<T>> P) {
         => new Parser<F>((input, index) => P(input, index) switch {
             Result<T>.Succ(var newIndex, var firstPattern) => next(firstPattern).P(input, newIndex) switch {
                 Result<S>.Succ(var lastIndex, var lastPattern) => new Result<F>.Succ(lastIndex, final(firstPattern, lastPattern)),
-                Result<S>.Fail(_, var message) => new Result<F>.Fail(index, message),
+                Result<S>.Fail(var failIndex, var message) => new Result<F>.Fail(failIndex, message),
                 _ => throw new Exception("Encountered unsupported Result in SelectMany(a)"),
             },
-            Result<T>.Fail(_, var message) => new Result<F>.Fail(index, message),
+            Result<T>.Fail(var failIndex, var message) => new Result<F>.Fail(failIndex, message),
             _ => throw new Exception("Encountered unsupported Result in SelectMany(b)"),
         });
 
     public Parser<T> Where(Func<T, bool> pred) 
         => new Parser<T>((input, index) => P(input, index) switch {
             Result<T>.Succ(var newIndex, var pattern) when pred(pattern) => new Result<T>.Succ(newIndex, pattern),
-            Result<T>.Succ(_, _) => new Result<T>.Fail(index, "parse successful; predicate failed"),
-            Result<T>.Fail(_, var message) => new Result<T>.Fail(index, $"parse failed in Where with {message}"),
+            Result<T>.Succ(var failIndex, _) => new Result<T>.Fail(failIndex, "parse successful; predicate failed"),
+            Result<T>.Fail(var failIndex, var message) => new Result<T>.Fail(failIndex, $"parse failed in Where with {message}"),
             _ => throw new Exception("Encountered unsupported Result in Where"),
         });
 
