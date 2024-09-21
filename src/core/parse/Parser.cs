42fb185f0cb332f2091019cc8b894bf10a5ba360
David L. Inman <davidlevere@gmail.com>
2024-09-21 16:44:00 -0400

    fix issue with failed parses passing on wrong index; make exception messages more unique; where

--- src/core/parse/Parser.cs
+++ src/core/parse/Parser.cs
@@ -16,19 +16,27 @@ public sealed record Parser<T>(Func<string, int, Result<T>> P) {
     public Parser<S> Select<S>(Func<T, S> map) 
         => new Parser<S>((input, index) => P(input, index) switch {
             Result<T>.Succ(var newIndex, var pattern) => new Result<S>.Succ(newIndex, map(pattern)),
-            Result<T>.Fail(var newIndex, var message) => new Result<S>.Fail(newIndex, message),
-            _ => throw new Exception("Encountered unsupported Result"),
+            Result<T>.Fail(_, var message) => new Result<S>.Fail(index, message),
+            _ => throw new Exception("Encountered unsupported Result in Select"),
         });
 
     public Parser<F> SelectMany<S, F>( Func<T, Parser<S>> next, Func<T, S, F> final ) 
         => new Parser<F>((input, index) => P(input, index) switch {
             Result<T>.Succ(var newIndex, var firstPattern) => next(firstPattern).P(input, newIndex) switch {
                 Result<S>.Succ(var lastIndex, var lastPattern) => new Result<F>.Succ(lastIndex, final(firstPattern, lastPattern)),
-                Result<S>.Fail(var lastIndex, var message) => new Result<F>.Fail(lastIndex, message),
-                _ => throw new Exception("Encountered unsupported Result"),
+                Result<S>.Fail(_, var message) => new Result<F>.Fail(index, message),
+                _ => throw new Exception("Encountered unsupported Result in SelectMany(a)"),
             },
-            Result<T>.Fail(var newIndex, var message) => new Result<F>.Fail(newIndex, message),
-            _ => throw new Exception("Encountered unsupported Result"),
+            Result<T>.Fail(_, var message) => new Result<F>.Fail(index, message),
+            _ => throw new Exception("Encountered unsupported Result in SelectMany(b)"),
+        });
+
+    public Parser<T> Where(Func<T, bool> pred) 
+        => new Parser<T>((input, index) => P(input, index) switch {
+            Result<T>.Succ(var newIndex, var pattern) when pred(pattern) => new Result<T>.Succ(newIndex, pattern),
+            Result<T>.Succ(_, _) => new Result<T>.Fail(index, "parse successful; predicate failed"),
+            Result<T>.Fail(_, var message) => new Result<T>.Fail(index, $"parse failed in Where with {message}"),
+            _ => throw new Exception("Encountered unsupported Result in Where"),
         });
 
     public Parser<T> Or(params Parser<T>[] other) => new Parser<T>((input, index) => {
