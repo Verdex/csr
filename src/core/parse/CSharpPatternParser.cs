36cd9f720514c4942de9a3eec6c1f9db11ca10f5
David L. Inman <davidlevere@gmail.com>
2024-09-21 17:27:57 -0400

    symbol and letter parsers

--- src/core/parse/CSharpPatternParser.cs
+++ src/core/parse/CSharpPatternParser.cs
@@ -25,6 +25,31 @@ public class CSharpPatternParser {
                 throw new Exception("Encountered unknown Result in CSharpPatternParser::TryParse");
         }
     }
-    */
+
+    private static Result<Pattern<string, CSharpAst>> Succ(int index, Pattern<string, CSharpAst> pattern) 
+        => new Result<Pattern<string, CSharpAst>>.Succ(index, pattern);
+    private static Result<Pattern<string, CSharpAst>> Fail(int index, string message) 
+        => new Result<Pattern<string, CSharpAst>>.Fail(index, message);
+
+    private static Parser<Unit> Letter(char c) => new Parser<Unit>((input, index) => { 
+        if(input[index] == c) {
+            return new Result<Unit>.Succ(index + 1, new Unit());
+        }
+        else {
+            return new Result<Unit>.Fail(index, $"expected {c} but found {input[index]}");
+        }
+    });
+
+    private static Parser<string> Symbol() => new Parser<string>((input, index) => { 
+        if (!char.IsAsciiLetter(input[index]) && input[index] != '_') {
+            return new Result<string>.Fail(index, $"expected symbol character but found {input[index]}");
+        }
+
+        var sym = new string(input.Skip(index).TakeWhile(c => char.IsAsciiLetterOrDigit(c) || c == '_').ToArray());
+
+        return new Result<string>.Succ(index + sym.Length, sym);
+    });
+
+    private sealed record Unit();
 }
 
